05e8c9d74483f541f7282fafbfad7e49b0903312
TabreaDavid <tabrea.david@icloud.com>
2024-12-10 22:57:12 +0200

    minor fix

---
+++ Semester 1/Fundamentals of programming/Assignment 8/src/ui/console.py
@@ -0,0 +1,100 @@
+EXIT = 0
+ADD_STUDENT = 1
+REMOVE_STUDENT = 2
+DISPLAY_STUDENTS = 3
+ADD_DISCIPLINE = 4
+REMOVE_DISCIPLINE = 5
+DISPLAY_DISCIPLINES = 6
+GRADE_STUDENT = 7
+DISPLAY_GRADES = 8
+SEARCH_STUDENT = 9
+SEARCH_DISCIPLINE = 10
+
+class Console:
+    def __init__(self, studentServices, disciplineServices, gradeServices):
+        self.studentServices = studentServices
+        self.disciplineServices = disciplineServices
+        self.gradeServices = gradeServices
+
+    @staticmethod
+    def printMenu():
+        print("1. Add student")
+        print("2. Remove student by ID")
+        print("3. Display students")
+        print("4. Add discipline")
+        print("5. Remove discipline by ID")
+        print("6. Display disciplines")
+        print("7. Grade student by ID's")
+        print("8. Display grades")
+        print("9. Search student by ID")
+        print("10. Search discipline by ID")
+        print(f"0. Exit\n")
+
+    def addStudentUi(self, studentId, studentName):
+        self.studentServices.addStudentService(studentId,studentName)
+
+    def removeStudentUi(self, removedStudendId):
+        self.studentServices.removeStudentService(removedStudendId)
+
+    def displayStudents(self):
+        self.studentServices.listStudents()
+
+    def addDisciplineUi(self, disciplineId, disciplineName):
+        self.disciplineServices.addDisciplineService(disciplineId, disciplineName)
+
+    def removeDisciplineUi(self, disciplineId):
+        self.disciplineServices.removeDisciplineService(disciplineId)
+
+    def displayDisciplines(self):
+        self.disciplineServices.listDisciplinesService()
+
+    def gradeStudentUi(self, studentId, disciplineId, gradeValue):
+        self.gradeServices.addGradeService(studentId, disciplineId, gradeValue)
+
+    def displayGrades(self):
+        self.gradeServices.listGradesService()
+    def runUi(self):
+        while True:
+            Console.printMenu()
+            userChoice = int(input("Your choice: "))
+
+            if userChoice == ADD_STUDENT:
+                studentId = int(input("Enter student ID: "))
+                studentName= input("Enter student name: ")
+                self.addStudentUi(studentId, studentName)
+
+            elif userChoice == REMOVE_STUDENT:
+                removedStudentId = int(input("Enter student ID: "))
+                self.removeStudentUi(removedStudentId)
+
+            elif userChoice == DISPLAY_STUDENTS:
+                self.displayStudents()
+
+            elif userChoice == ADD_DISCIPLINE:
+                disciplineId = int(input("Enter discipline id: "))
+                disciplineName = input("Enter discipline name: ")
+                self.addDisciplineUi(disciplineId, disciplineName)
+
+            elif userChoice == REMOVE_DISCIPLINE:
+                removedDisciplineId = int(input("Enter discipline id: "))
+                self.removeDisciplineUi(removedDisciplineId)
+
+            elif userChoice == DISPLAY_DISCIPLINES:
+                self.displayDisciplines()
+
+            elif userChoice == GRADE_STUDENT:
+                studentId = int(input("Enter student id: "))
+                disciplineId = int(input("Enter discipline id: "))
+                gradeValue = int(input("Enter the grade: "))
+                self.gradeStudentUi(studentId, disciplineId, gradeValue)
+
+            elif userChoice == DISPLAY_GRADES:
+                self.displayGrades()
+
+            elif userChoice == EXIT:
+                break
+
+            else: print("Your option is not in the menu!")
+
+
+
